06a98e41690d36a3e1cec07a4f865baf7a25857f
mychen08 <33041901+mychen08@users.noreply.github.com>
2017-11-25 01:35:42 -0800

    added motor
    sensor still bugged though

--- arduinoMain/arduinoMain.ino
+++ arduinoMain/arduinoMain.ino
@@ -1,13 +1,22 @@
-// Currently copied from lab. Needs to be modded for project
+ // Currently copied from lab. Needs to be modded for project
 
 
 // Use the library Softwareserial to use pins 2 and 3 of the arduino for serial comunication with the bluetooth unit
 // pins 0 and 1 are not use to be able to lead the program and read the program output on the Arduino serial monitor
 
 #include<SoftwareSerial.h>
+#include <Sparkfun_DRV2605L.h>
+#include <Wire.h>
+
+SFE_HMD_DRV2605L HMD;
+const int analogInPin = A0;  // Analog input pin that the sensor is attached to
+const int analogOutPin = 9; // Analog output pin that the Haptic Motor Driver is attached to
 
 int bluetoothTx = 2;
 int bluetoothRx = 3;
+
+int sensorValue = 0;        // value read from the sensor
+int outputValue = 0;        // value output to the PWM (analog out)
 
 
 // store voltages from thumb and index pressure sensors
@@ -33,7 +42,12 @@ void setup() {
 Serial.begin(9600);
 
 // Setup Bluetooth serial connection by programming the unit to communicate at 9600 bauds
-
+  HMD.begin();
+  
+  HMD.Mode(0x03); //PWM INPUT 
+  HMD.MotorSelect(0x0A);
+  HMD.Library(7); //change to 6 for LRA motors 
+  
   bluetooth.begin(115200);
   bluetooth.print("$");
   bluetooth.print("$");
@@ -66,6 +80,20 @@ void loop() {
   // Read pressure sensor voltages
   // Thumb = A0
   // Index = A1
+
+   // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  // change the analog out value:
+  if(outputValue < 170)
+    analogWrite(analogOutPin, outputValue);
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
   
   pressureThumb = analogRead(A0);
   pressureThumbVolt = float(pressureThumb)*3.0/1023.0;
